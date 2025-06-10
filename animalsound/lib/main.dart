ef6bc61d540fb367df881af293a79cd8e33e7f74
Lse-Yeong <ocdee39@gmail.com>
2025-06-10 22:49:01 +0900

    8장 동물 소리 앱 구현 완료

--- animalsound/lib/main.dart
+++ animalsound/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Animal Sounds",
       theme: ThemeData(
         colorSchemeSeed: Colors.blue,
@@ -22,6 +25,11 @@ class MyApp extends StatelessWidget {
 class AnimalSounds extends StatelessWidget {
   const AnimalSounds({super.key});
 
+  void playSound(String name) {
+    final AudioPlayer player = AudioPlayer();
+    player.play(AssetSource('$name.wav'));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,8 +37,203 @@ class AnimalSounds extends StatelessWidget {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text("Animal Sounds"),
         centerTitle: true,
+        leading: SizedBox(
+          height: 50,
+          width: 150,
+          child: Lottie.asset("lottie/cat.json"),
+        ),
       ),
-      body: Column()
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  children: [
+                    GestureDetector(
+                      onTap:() {
+                        playSound("bear");
+                      },
+                      child: Image.asset(
+                        "images/bear.png",
+                        height: 100,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10,bottom: 10),
+                      child: Container(
+                        width: 115,
+                        height: 2,
+                        color: Colors.green,
+                      ),
+                    ),
+                    const Text(
+                      "Bear",
+                      style: TextStyle(
+                        fontSize: 20
+                      ),
+                    )
+                  ],
+                ),
+                Column(
+                  children: [
+                    GestureDetector(
+                      onTap:() {
+                        playSound("fox");
+                      },
+                      child: Image.asset(
+                        "images/fox.png",
+                        height: 100,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10,bottom: 10),
+                      child: Container(
+                        width: 115,
+                        height: 2,
+                        color: Colors.green,
+                      ),
+                    ),
+                    const Text(
+                      "Fox",
+                      style: TextStyle(
+                        fontSize: 20
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        playSound("koala");
+                      },
+                      child: Image.asset(
+                        "images/koala.png",
+                        height: 100,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10,bottom: 10),
+                      child: Container(
+                        width: 115,
+                        height: 2,
+                        color: Colors.purple,
+                      ),
+                    ),
+                    const Text(
+                      "Koala",
+                      style: TextStyle(
+                        fontSize: 20
+                      ),
+                    )
+                  ],
+                ),
+                Column(
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        playSound("camel");
+                      },
+                      child: Image.asset(
+                        "images/camel.png",
+                        height: 100,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10,bottom: 10),
+                      child: Container(
+                        width: 115,
+                        height: 2,
+                        color: Colors.purple,
+                      ),
+                    ),
+                    const Text(
+                      "Camel",
+                      style: TextStyle(
+                        fontSize: 20
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 50),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        playSound("lion");
+                      },
+                      child: Image.asset(
+                        "images/lion.png",
+                        height: 100,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10,bottom: 10),
+                      child: Container(
+                        width: 115,
+                        height: 2,
+                        color: Colors.red,
+                      ),
+                    ),
+                    const Text(
+                      "Lion",
+                      style: TextStyle(
+                        fontSize: 20
+                      ),
+                    )
+                  ],
+                ),
+                Column(
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        playSound("tiger");
+                      },
+                      child: Image.asset(
+                        "images/tiger.png",
+                        height: 100,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10,bottom: 10),
+                      child: Container(
+                        width: 115,
+                        height: 2,
+                        color: Colors.red,
+                      ),
+                    ),
+                    const Text(
+                      "Tiger",
+                      style: TextStyle(
+                        fontSize: 20
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      )
     );
   }
 }
