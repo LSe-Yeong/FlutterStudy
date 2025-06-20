a9bb460bf5c78a999b7aedb5bc8a23c2ac97f537
Lse-Yeong <ocdee39@gmail.com>
2025-06-20 10:44:04 +0900

    15장 프로젝트 세팅

---
+++ weather/lib/screen/loading.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Loading extends StatefulWidget {
+  const Loading({super.key});
+
+  @override
+  State<Loading> createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      body: Center(
+        child: FilledButton(
+          onPressed: (){}, 
+          child: const Text("Get location"),
+        ),
+      ),
+    );
+  }
+}
